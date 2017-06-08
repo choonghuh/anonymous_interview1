db7a3127cd2095982b64159b4a6b9f799ba6413e
Choong Huh <choonghuh@gmail.com>
2017-06-07 23:01:38 -0700

    I think it works? Now to preserve metadata.

--- ex.sh
+++ ex.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-echo "tar unzipping $1"
-var="$( tar -xzvf $1 )"
+# ex.sh
+# Author: Choong Huh
+# Description:
+# The program unzips user-provided gzip file, which shall contain one or more 
+# text files that contain logging data.
+# Redacted copies of the logs are created by redact.py, along with audit.txt
+# and the resulting files are compressed back into output.tar.gz
 
+echo "tar unzipping $1"
+
+# Saving tar extraction stdout output to var
+var="$( tar -xzvf $1 )"
+var2=""
+rm -f auditLog.txt
+touch auditLog.txt
+
+# Building string var2 to be used in compressing and cleanup
 for a in $var; do
 	python redact.py $a
+	rm $a
+	var2="$var2 $a.redact"
 done
+# echo $var2
+
+tar -czvf output.tar.gz auditLog.txt $var2
+rm auditLog.txt $var2
+# TODO - Carry over metadata
--- redact.py
+++ redact.py
@@ -1,15 +1,30 @@
+# redact.py
+# Author: Choong Huh
+# Description:
+# The program will read each line of the file given by ex.sh script.
+# If sensitive information is found, the occurence is recorded in the audit file
+# and the offending line is removed.
+# Otherwise, the line is written to a new file which serves as the redacted copy.
+
 import sys
 
 def redact(sourceFile):
-	redactedOutFile = open(sourceFile+'.redac', 'w')
+	redactedOutFile = open(sourceFile+'.redact', 'w')
+	auditOutFile = open('auditLog.txt', 'a')
 	# TODO - open an audit file to append activity log per redact()
 	with open(sourceFile) as inputFile:
+		ssi_counter = 0
 		for line in inputFile:
 			if ('CC=' not in line) and ('SSN=' not in line):
 				redactedOutFile.write(line)
+			else:
+				ssi_counter += 1
+		auditOutFile.write('SSI Occurences in {0} : {1}\n'.format(sourceFile, str(ssi_counter)))
 
 	redactedOutFile.close()
 	inputFile.close()
+	auditOutFile.close()
 
 if __name__ == '__main__':
+	# Depends on ex.sh to always give one argument
 	redact(sys.argv[1])
